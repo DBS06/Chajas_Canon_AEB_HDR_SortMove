e896406cb20c475dfd8ea0adc918e629e9b7cd37
Philipp Steiner <philipp.steiner1987@gmail.com>
2020-01-31 23:17:10 +0100

    parse exif config & some globals

--- HDR_Move.py
+++ HDR_Move.py
@@ -26,6 +26,15 @@ DEFAULT_STOP_TAG = ""
 print_img_plh = 13
 print_tag_plh = 23
 
+# Global Variables
+searchDir = ""
+image_file_ending = ""
+imgFiles = []
+hdr_move_list = deque()
+hdr_skip_list = deque()
+hdr_count = 0
+hdr_list = []
+
 
 class ExifTag(object):  # pylint: disable=too-few-public-methods
     """
@@ -44,18 +53,42 @@ def main():  # pragma: no cover
     parsing commandline args
     """
 
+    print("####################################")
+    print("# Chaja's - Canon AEB HDR SortMove #")
+    print("####################################")
+
     parser = create_parser()
     args = parser.parse_args()
+
+    if args.path == '':
+        # Get path to image folder and proof if it exists
+        while (1):
+            directory = input("Path to Image Folder (abort with hit 'RETURN'): ")
+            # directory = "C:\Users\phst\Pictures\Test_Pictures"
+            if (directory == ""):
+                sys.exit("Exit by User!")
+
+            if (os.path.exists(directory)):
+                searchDir = directory
+                break
+            else:
+                print("invalid path!")
+    else:
+        if (os.path.exists(args.path)):
+            searchDir = args.path
+        else:
+            sys.exit("invalid path!")
+
+    print(searchDir)
+
     default_cfg_file_path = os.path.dirname(os.path.realpath(__file__)) + '/' + CDG_FILE_NAME
     config = load_json_cfg(default_cfg_file_path)
 
-    print(args.path)
-    if args.yes:
-        print("auto yes")
-    else:
-        print("ask")
+    # TODO: check which mode should be used
+    parseGeneralCfg(config)
 
-    print(config)
+    # hdr_move_lise =
+    getExifHdrList(config)
 
 
 #############################
@@ -96,12 +129,43 @@ def create_parser():  # pragma: no cover
         description='This script helps to sort HDR images from Canon Cameras')
 
     main_parser.add_argument("-p", "--path", action="store", default='', help="path to image folder")
-    main_parser.add_argument("-m", "--mode", action="store", default='auto',
+    main_parser.add_argument("-m", "--mode", action="store", default='-',
                              help="defines if HDR-Search is based on exif-data, txt-files, or decide based on folders content: <auto/exif/txt>")
     main_parser.add_argument("-y", "--yes", action="store_true", default='',
                              help="sorting starts automatically, no asking!")
 
     return main_parser
+
+
+def parseGeneralCfg(config):
+    image_file_ending = config["general"]["imageSrcFileEnding"]
+
+
+def getExifHdrList(config):
+    # HDR-Sequence Stuff
+    hdr_sequence_num_min = config["exifModeCfg"]["hdrSequenceMinNum"]
+
+    # EXIF-Tags which indicates that a picture is part of a HDR-Sequence:
+    # primary-tag is used to find the images which are part of an HDR-Sequence
+    # this tag should be part of 'Standard'-EXIF-Tags to increase the search speed, otherwise it would be really slow
+    exif_primary_tag = ExifTag(config["exifModeCfg"]["primaryTag"]["name"],
+                               config["exifModeCfg"]["primaryTag"]["value"])
+
+    # secondary- and tertiary-tag are used to find the beginning of a HDR-Sequence
+    exif_secondary_tag = ExifTag(config["exifModeCfg"]["secondaryTag"]["name"],
+                                 config["exifModeCfg"]["secondaryTag"]["value"])
+    exif_tertiary_tag = ExifTag(config["exifModeCfg"]["tertiaryTag"]["name"],
+                                config["exifModeCfg"]["tertiaryTag"]["value"])
+
+    print("## EXIF-Mode")
+    print("")
+    print("## Settings:")
+    print("# Primary EXIF-Tag:            " + exif_primary_tag.name + " -> Value: " + str(exif_primary_tag.val))
+    print("# Secondary EXIF-Tag:          " + exif_secondary_tag.name + " -> Value: " + str(exif_secondary_tag.val))
+    print("# Tertiary EXIF-Tag:           " + exif_tertiary_tag.name + " -> Value: " + str(exif_tertiary_tag.val))
+    print("# Search Images:               *" + image_file_ending)
+    print("# Min Images for HDR-Sequence: " + str(hdr_sequence_num_min))
+    print("")
 
 
 if __name__ == "__main__":  # pragma: no cover
